4cc8b2aaf00060b2fec2d1321160446ac918424f
walkingfeet <dmitry.nazarenko.v@gmail.com>
2014-10-04 16:38:05 +0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   match '/help', to: "static_pages#help", via: 'get'
   match '/about', to: "static_pages#about", via: 'get'
   match '/contact', to: "static_pages#contact", via: 'get'
