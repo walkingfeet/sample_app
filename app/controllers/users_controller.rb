e13b4316eb7b4b605d9efaeef7109c58616748c4
walkingfeet <dmitry.nazarenko.v@gmail.com>
2014-10-22 14:21:34 +0400

    Commit branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -14,12 +21,38 @@ class UsersController < ApplicationController
     end
   end
   
-  def user_params
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+  end
+  
+  private 
+  
+   def user_params
      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
   end
   
-  def show
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in"
+    end  
+  end
+  
+  def correct_user
     @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
