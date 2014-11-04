b8f2e1686e2db3a6cad8f6989a3ccfb084fb8133
walkingfeet <dmitry.nazarenko.v@gmail.com>
2014-11-05 01:30:17 +0300

    Finished the book

--- spec/requests/authentification_pages_spec.rb
+++ spec/requests/authentification_pages_spec.rb
@@ -51,6 +51,8 @@ describe "Authentication" do
     describe "for non-signed-in users"  do
       let(:user) { FactoryGirl.create(:user) }
       
+    
+      
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -87,6 +89,16 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
       
      describe "as wrong user" do
@@ -105,6 +117,19 @@ describe "Authentication" do
          specify { expect(response).to redirect_to(root_url) }
        end
      end
+      
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
   end
   
